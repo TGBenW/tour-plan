cc958a4902ef94626ff89ca7fc5c052a3fd55ec4
TGBenW <dukariga@gmail.com>
2021-05-12 02:01:56 +0300

    create: yandex map added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
   },
   keyboard: {
     enabled: true,
   },
+});
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map("first_map", {
+    center: [-4.77, 55.52],
+    zoom: 11,
+  });
+  // Создание геообъекта с типом точка (метка).
+  var myGeoObject = new ymaps.GeoObject({
+    geometry: {
+      type: "Point", // тип геометрии - точка
+      coordinates: [-4.77, 55.52], // координаты точки
+    },
+  });
+
+  // Размещение геообъекта на карте.
+  myMap.geoObjects.add(myGeoObject);
 });
