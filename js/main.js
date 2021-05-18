5f3eb6f1e645e1045158dd9cf0a4c203d1856458
TGBenW <dukariga@gmail.com>
2021-05-18 23:05:49 +0300

    create: navbar mobile

--- js/main.js
+++ js/main.js
@@ -25,3 +25,10 @@ const reviewsSlider = new Swiper(".reviews-slider", {
     enabled: true,
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("menu button clicked");
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  document.querySelector("#body").classList.toggle("oh");
+});
