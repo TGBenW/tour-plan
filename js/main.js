5ff475447b8169020726f53a1acecef5e226c636
TGBenW <dukariga@gmail.com>
2021-05-21 14:52:32 +0300

    create: modal+responsive

--- js/main.js
+++ js/main.js
@@ -1,34 +1,54 @@
-const hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  const hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+    },
+  });
 
-const reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+    },
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("menu button clicked");
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
-  document.querySelector("#body").classList.toggle("oh");
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+    $("#body").toggleClass("oh");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  modalButton.on("click", openModal);
+  var closeModalButton = $(".modal__close");
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
